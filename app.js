9265253a6b64f90d675aa13d9c9d43d9a71aa747
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-18 14:52:50 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000")
+})
